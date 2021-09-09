03514d410577e26b054884359f1f71d24ee89b4d
bacha-11 <ijazbacha14@gmail.com>
2021-09-09 16:53:00 +0500

    send function in props

--- src/IfElse.js
+++ src/IfElse.js
@@ -1,12 +1,26 @@
 import { useState } from "react";
+import { Logout } from "./Logout";
+
 
 function Condition(){
     let [login, setlogin] = useState(true)
 
+    function SendFun(){
+        alert('send data')
+    }
+
     return(
-        login?
-        <h1>hello user</h1>
-        :<h1>hello guest</h1>
+        <div>
+            login?
+            <h1>hello user</h1>
+            :<h1>hello guest</h1>
+
+            <div>
+                <Logout data={SendFun} />
+            </div>
+        </div>
+
+
     )
 }
 
--- src/Logout.js
+++ src/Logout.js
@@ -1,9 +1,11 @@
-export function Logout(){
+export function Logout(props){
     return(
         <div>
             <h1>
-                Logout component
+                Logout component 
             </h1>
+            <button onClick={props.data}>get data</button>
+            
         </div>
     )
 }
