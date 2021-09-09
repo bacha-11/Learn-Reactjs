0ce43ec1518bce6a69191cecc5eed3e66942fdf5
bacha-11 <ijazbacha14@gmail.com>
2021-09-09 10:00:12 +0500

    Function and class component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './Login';
+import { Logout } from './Logout';
+import User from './User';
+import Home from './Home';
+import ClassCom from './ClassCom';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello World</h1>
+      <Login />
+      <Logout />
+      <User />
+      <Home />
+      <ClassCom />
     </div>
   );
 }
---
+++ src/Logout.js
@@ -0,0 +1,9 @@
+export function Logout(){
+    return(
+        <div>
+            <h1>
+                Logout component
+            </h1>
+        </div>
+    )
+}
