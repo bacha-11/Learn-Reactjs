57747f560347ad50305595518fbbae302dc28af7
bacha-11 <ijazbacha14@gmail.com>
2021-09-15 15:08:57 +0500

    hooks- UseEffect hook with condition in react js

--- src/UseEffectHook.js
+++ src/UseEffectHook.js
@@ -6,14 +6,17 @@ import ComponentDidUpdateExample from "./ComponentDidUpdate"
 // useEffect hook is work like ComponentDidMount and ComponentDidUpdate
 function UseEffectHook(){
     let [count, setCount] = useState(0)
+    let [data, setData] = useState(10)
+
     useEffect(()=>{
         console.log('UseEffectHook')
-    })
+    },[count])
 
     return(
         <div>
-            <h1>UseEffectHook {count}</h1>
+            <h1>UseEffectHook count-{count}, data-{data}</h1>
             <button onClick={()=>{setCount(count+1)}}>Update Count</button>
+            <button onClick={()=>{setData(data+1)}}>Update Data</button>
         </div>
     )
 }
