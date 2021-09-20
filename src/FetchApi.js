663204dd2d746563e538fc92c483c37964416de2
bacha-11 <ijazbacha14@gmail.com>
2021-09-20 13:54:27 +0500

    full-fill form in react js

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -5,6 +5,8 @@ import { Link } from "react-router-dom";
 
 function FetchApi(){
     let [user, setUser] = useState([])
+    let [title, setTitle] = useState("")
+    let [check, setCheck] = useState()
 
     useEffect(()=>{
         getList()
@@ -17,6 +19,8 @@ function FetchApi(){
             result.json().then((resp)=>{
                 // console.log('result********', resp)
                 setUser(resp)
+                // setTitle(resp[0].title)
+                // setCheck(resp[0].completed)
             })
         })
     }
@@ -31,6 +35,14 @@ function FetchApi(){
                 getList()
             })
         })
+    }
+
+
+    function updateData(id){
+        let item = user[id-1]
+        console.log(item)
+        setTitle(item.title)
+        setCheck(item.completed)
     }
 
     return(
@@ -52,11 +64,18 @@ function FetchApi(){
                             <td><Link to={"/user/"+item.id}>{item.title}</Link></td>
                             <td>{item.completed}</td>
                             <td><Button onClick={()=>deleteData(item.id)}>Delete</Button></td>
+                            <td><Button onClick={()=>updateData(item.id)}>Update</Button></td>
                         </tr>
                         )
                     }
                 </tbody>
             </Table>
+
+            <div>
+                <input type='text' value={title} name="title"></input><br />
+                <input type='checkbox' value={check} name="completed"></input><br />
+                <Button>Update</Button>
+            </div>
         </div>
     )
 }
