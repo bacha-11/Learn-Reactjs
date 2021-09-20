9e4f676d0d519faf91a28f85053eed02c52a1a2e
bacha-11 <ijazbacha14@gmail.com>
2021-09-20 12:17:22 +0500

    Api delete method in react js

--- src/FetchApi.js
+++ src/FetchApi.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Table } from "react-bootstrap";
+import { Button, Table } from "react-bootstrap";
 import { Link } from "react-router-dom";
 
 
@@ -7,13 +7,31 @@ function FetchApi(){
     let [user, setUser] = useState([])
 
     useEffect(()=>{
+        getList()
+    })
+
+
+    // use this function to call when data has been delete so we cant refesh the page manully 
+    function getList(){
         fetch('https://jsonplaceholder.typicode.com/todos').then((result)=>{
             result.json().then((resp)=>{
                 // console.log('result********', resp)
                 setUser(resp)
             })
         })
-    })
+    }
+
+
+    function deleteData(id){
+        fetch(`https://jsonplaceholder.typicode.com/todos/${id}`,{
+            method:'DELETE'
+        }).then((result)=>{
+            result.json().then((resp)=>{
+                console.log(resp)
+                getList()
+            })
+        })
+    }
 
     return(
         <div>
@@ -25,6 +43,7 @@ function FetchApi(){
                         <th>ID</th>
                         <th>Title</th>
                         <th>Completed</th>
+                        <th>Delete</th>
                     </tr>
                     {
                         user.map((item)=>
@@ -32,6 +51,7 @@ function FetchApi(){
                             <td>{item.id}</td>
                             <td><Link to={"/user/"+item.id}>{item.title}</Link></td>
                             <td>{item.completed}</td>
+                            <td><Button onClick={()=>deleteData(item.id)}>Delete</Button></td>
                         </tr>
                         )
                     }
